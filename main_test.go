a84683df42555fb0264c3d47397ea1bb15ff0178
Kamil16345 <75177619+Kamil16345@users.noreply.github.com>
2021-07-07 13:48:14 +0200

    Add tests for handlers

--- main_test.go
+++ main_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strconv"
 	"testing"
 )
 
@@ -117,4 +121,66 @@ func TestStaticFileServer(t *testing.T){
 	if expectedContentType != contentType{
 		t.Errorf("Wrong content type, expected $s, got %s", expectedContentType, contentType)
 	}
+}
+func TestGetDogHandler(t *testing.T){
+	dogs=[]Dog{
+		{"Labrador retriever", "Medium-size dog"},
+	}
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil{
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	hf := http.HandlerFunc(getDogHandler)
+	hf.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status!= http.StatusOK{
+		t.Errorf("handler returned wrong status code, get %v, want %v", status, http.StatusOK)
+	}
+	expected := Dog{"Labrador retriever", "Medium-size dog"}
+	b := []Dog{}
+	err =json.NewDecoder(recorder.Body).Decode(&b)
+
+	if err!= nil{
+		t.Fatal(err)
+	}
+	actual := b[0]
+	if actual != expected{
+		t.Errorf("handler returned unexpected body, got %v want %v",actual, expected)
+	}
+}
+func TestCreateDogsHandler(t *testing.T){
+	dogs = []Dog{
+		{"Labrador retriever", "Medium-size dog"},
+	}
+	form := newCreateDogForm()
+	req, err := http.NewRequest("POST", "", bytes.NewBufferString(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+
+	if err != nil{
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+
+	hf := http.HandlerFunc(createDogHandler)
+	hf.ServeHTTP(recorder, req)
+	if status := recorder.Code; status != http.StatusFound{
+		t.Errorf("handler returned wrong status code: got%v, want %v", status, http.StatusOK)
+	}
+	expected := Dog{"Labrador retriever", "Medium-size dog"}
+
+	if err != nil{
+		t.Fatal(err)
+	}
+	actual := dogs[1]
+	if actual != expected{
+		t.Errorf("handler returned unexpected body: got %v want %v",actual, expected)
+	}
+}
+func newCreateDogForm() *url.Values{
+	form := url.Values{}
+	form.Set("species", "eagle")
+	form.Set("description", "Medium-size dog")
+	return &form
 }
